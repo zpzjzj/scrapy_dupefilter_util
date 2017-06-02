8ffda60c3c19ca07b1e419110072a86d55269b1d
zpzjzj <zpzjzj@sina.cn>
2017-06-02 14:57:16 +0800

    添加对 elasticsearch 的支持

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# 过滤重复 item 的中间件
+# scrapy 下 item 重复过滤器
+scrapy 框架下，通过设置和检查 item 的特征项，避免重复写入数据库或重复爬取。
+
 主要分为：
 1. `ItemRequestDupeFilter` 请求过滤中间件，根据当前局部提取的 item 的特征项 (key) 查找是否已在数据库中存在，若有，则过滤掉该请求。
-
++ 存在目的：在爬虫爬取过程中，通过对爬虫请求中已抽取的局部特征项进行匹配检查是否已在数据库中存在，及时止损。适用于对临时链接的爬取，间隔时间较长的增量爬取。
 2. `DupefilterPipeline` 导入到数据库的中间件，检查数据库中是否已有该 item 项，若有且无需更新，则不再加入到数据库中。
+   + 存在目的：保证爬虫的多次运行在数据库中只留一份实例。
 
 
-目前用于 MongoDB (**TODO**：适配 Elastic Search)
+目前可用于 MongoDB, ElasticSearch
 
 
 ## 通用设置
@@ -24,7 +27,7 @@ Item 类中定义 item 对象的特征字段和待补全字段:
 
 ```python
 class <item_class>(scrapy.Item):
-	<key_field> = scrapy.Field(key = True) # key 用于标识 item 的特征，用于数据库的查找
+	<key_field> = scrapy.Field(key = True)  # key 用于标识 item 的特征，用于数据库的查找
 	<field> = scrapy.Field(nullable = True) # nullable 表明该字段可在初期为 null, 之后更新内容。用于由于反爬虫等原因导致初次爬取时数据不全的情况的更新补充。
 ```
 
@@ -51,3 +54,31 @@ ITEM_PIPELINES = {
 }
 DUPEFILTER_PIPELINE_CONFIG: {'items': [{'item': <item_class>, 'collection': <collection> }]}
 ```
+### ElasticSearch 过滤
+基于 [scrapy-elasticsearch](https://github.com/noplay/scrapy-elasticsearch) 建立对各个 item 项的单独配置支持和检查。
+
+路径分别改为 `scrapy_dupefilter_util.es.ItemRequestDupeFilter` 和 `scrapy_dupefilter_util.es.DupefilterPipeline`
+
+参数：
+
++ ELASTICSEARCH_INDEX
++ ELASTICSEARCH_TYPE
++ ELASTICSEARCH_UNIQ_KEY (optional) item 的特征字段，可用于 hash 生成唯一的文档 _id
+
+
+参数可在全局的 settings 中设置，也可以对各个 item 项设置。如
+
+```
+item = {'item': <item_class>, 'collection': <collection>, 'keys': [<field1>, <field2>], 'nullable_fields': [<field>], 'ELASTICSEARCH_UNIQ_KEY': <uniq_key>}
+DUPEFILTER_PIPELINE_CONFIG: {'items': [item]}
+```
+
+
+
+TODO:
+
+[ ] `nullable_fields` 在请求过滤期间的检查
+
+[ ] 代码重构，减少冗余重复
+
+[ ] 统一 mongodb 过滤器中的参数，添加对全局默认参数的设置
---
+++ es/utils.py
@@ -0,0 +1,220 @@
+from scrapy.dupefilters import RFPDupeFilter
+from scrapy.utils.job import job_dir
+import logging
+from scrapyelasticsearch.scrapyelasticsearch import ElasticSearchPipeline
+
+logger = logging.getLogger(__name__)
+
+"""
+SETTINGS
+
+# 这里路径需要填补完全，否则会出现 item 和 ItemClass 类型不匹配的情况
+from {project}.items import {ItemClass}
+
+ELASTICSEARCH_SERVERS = ['http://localhost:9200']
+ELASTICSEARCH_INDEX = 'domain'
+ELASTICSEARCH_UNIQ_KEY = None
+
+ITEM_PIPELINES = {
+  'scrapy_dupefilter_util.es.DupefilterPipeline' : 300
+}
+
+DUPEFILTER_PIPELINE_CONFIG: {'items': [{'item': <item_class>, 'collection': <collection> }]}
+
+DUPEFILTER_CLASS = "scrapy_dupefilter_util.es.ItemRequestDupeFilter"
+REQUEST_DUPEFILTER_CONFIG: {'items': [{'item': <item_class>, 'collection': <collection> }]}
+
+in Item class:
+
+key = True
+nullable = True
+
+"""
+
+DUPEFILTER_PIPELINE_CONFIG = "DUPEFILTER_PIPELINE_CONFIG"
+REQUEST_DUPEFILTER_CONFIG = "REQUEST_DUPEFILTER_CONFIG"
+
+
+def get_item_dict(items, settings):
+    def extract_keys(cls, attr_name='key'):
+        return [field for field, attr in cls.fields.items() if attr.get(attr_name)]
+    # set attributes for each item class
+    res = dict()  # item cls => attributes (keys, nullable_fields, es_index, es_type, es_unique_keys ...)
+    default_index = settings.get('ELASTICSEARCH_INDEX')
+    default_type = settings.get('ELASTICSEARCH_TYPE')
+    default_uniq_key = settings.get('ELASTICSEARCH_UNIQ_KEY')
+    for item in items:
+        val = item.copy()
+        item_cls = item['item']
+        val['keys'] = item.get('keys') or extract_keys(item_cls)
+        val['nullable_fields'] = item.get('nullable_fields') or extract_keys(item_cls, 'nullable')
+        val['ELASTICSEARCH_INDEX'] = item.get('ELASTICSEARCH_INDEX') or default_index
+        val['ELASTICSEARCH_TYPE'] = item.get('ELASTICSEARCH_TYPE') or default_type
+        val['ELASTICSEARCH_UNIQ_KEY'] = item.get('ELASTICSEARCH_UNIQ_KEY') or default_uniq_key
+        res[item_cls] = val
+    return res
+
+
+def check_settings(settings):
+    var_str = ['MONGO_URI', 'MONGO_DATABASE']
+    for var in var_str:
+        setting = settings.get(var)
+        if not setting:
+            logging.error("{} not set in settings".format(var))
+        else:
+            logging.debug(f'in check_settings: {var} = {setting}')
+
+
+def search_item(es, item, cls_info_res):
+    keys = cls_info_res['keys']
+    index = cls_info_res.get('ELASTICSEARCH_INDEX')
+    type_name = cls_info_res.get('ELASTICSEARCH_TYPE')
+    if not es.indices.exists_type(index, type_name):
+        return None
+    query = {
+        "query": {
+            "constant_score": {
+                "filter": {
+                    "bool": {
+                        "must": [{"match": {key: item.get(key)}} for key in keys]
+                    }
+                }
+            }
+        }
+    }
+    res = es.search(index=index, doc_type=type_name, body=query)
+    if res['hits']['total'] <= 0:
+        return None
+    else:
+        item = res['hits']['hits'][0]['_source']
+        item['_id'] = res['hits']['hits'][0]['_id']
+        return item
+
+
+class ItemRequestDupeFilter(RFPDupeFilter):
+    settings = None
+    es = None
+    items = dict()
+
+    def request_seen(self, request):
+        fp_seen = super(ItemRequestDupeFilter, self).request_seen(request)
+        if fp_seen:
+            return fp_seen
+        item = request.meta.get('item')
+        for item_cls, cls_info in self.items.items():
+            if isinstance(item, item_cls) and cls_info['keys']:
+                cls_info_res = cls_info
+                break
+        else:
+            return
+        keys = cls_info_res['keys']
+        key_dict = {key: item.get(key) for key in keys}
+        if keys and key_dict:
+            res = search_item(self.es, item, cls_info_res)
+            return res is not None
+
+    @classmethod
+    def from_settings(cls, settings):
+        from elasticsearch import Elasticsearch
+        check_settings(settings)
+        debug = settings.getbool('DUPEFILTER_DEBUG')
+        config = settings.getdict('REQUEST_DUPEFILTER_CONFIG', {})
+        obj = cls(path=job_dir(settings), debug=debug)
+        obj.settings = settings
+        es_servers = obj.settings['ELASTICSEARCH_SERVERS']
+        es_servers = es_servers if isinstance(es_servers, list) else [es_servers]
+        obj.items = get_item_dict(config.get('items'), settings)
+        obj.es = Elasticsearch(hosts=es_servers, timeout=obj.settings.get('ELASTICSEARCH_TIMEOUT', 60))
+        return obj
+
+
+class DupefilterPipeline(ElasticSearchPipeline):
+    """
+        apply to one kind of item only
+    """
+    items = dict()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        ext = super(DupefilterPipeline, DupefilterPipeline).from_crawler(crawler)
+        check_settings(crawler.settings)
+        ext.items = get_item_dict(crawler.settings.get('DUPEFILTER_PIPELINE_CONFIG').get('items'), crawler.settings)
+        return ext
+
+    def search_item(self, item, cls_info_res):
+        keys = cls_info_res['keys']
+        index = cls_info_res.get('ELASTICSEARCH_INDEX')
+        type_name = cls_info_res.get('ELASTICSEARCH_TYPE')
+        if not self.es.indices.exists_type(index, type_name):
+            return None
+        query = {
+            "query": {
+                "constant_score": {
+                    "filter": {
+                        "bool": {
+                            "must": [{"match": {key: item.get(key)}} for key in keys]
+                        }
+                    }
+                }
+            }
+        }
+        res = self.es.search(index=index, doc_type=type_name, body=query)
+        if res['hits']['total'] <= 0:
+            return None
+        else:
+            item = res['hits']['hits'][0]['_source']
+            item['_id'] = res['hits']['hits'][0]['_id']
+            return item
+
+    def bulk_item(self, item, cls_info_res, operation='index'):
+        is_index = operation == 'index'
+        index_name = cls_info_res.get('ELASTICSEARCH_INDEX')
+        index_action = {
+            '_index': index_name,
+            '_type': cls_info_res.get('ELASTICSEARCH_TYPE'),
+            '_source' if is_index else 'doc': item,
+            '_op_type': operation,  # python 库的接口方式和 api 不一致，angry！
+        }
+        unique_key = cls_info_res.get('ELASTICSEARCH_UNIQ_KEY') or self.settings['ELASTICSEARCH_UNIQ_KEY']
+        if is_index and unique_key is not None:
+            item_unique_key = item[self.settings['ELASTICSEARCH_UNIQ_KEY']]
+            unique_key = self.get_unique_key(item_unique_key)
+            import hashlib
+            item_id = hashlib.sha1(unique_key).hexdigest()
+            index_action['_id'] = item_id
+            logging.debug('Generated unique key %s' % item_id)
+        else:
+            index_action['_id'] = item.pop('_id')
+
+        self.items_buffer.append(index_action)
+        # print(str(self.items_buffer).replace('\'', '\"'))
+        self.send_items()
+        if len(self.items_buffer) >= self.settings.get('ELASTICSEARCH_BUFFER_LENGTH', 500):
+            self.send_items()
+            self.items_buffer = []
+
+    def process_item(self, item, spider):
+        import types
+        if isinstance(item, types.GeneratorType) or isinstance(item, list):
+            for each in item:
+                self.process_item(each, spider)
+        else:
+            for item_cls, cls_info in self.items.items():
+                if isinstance(item, item_cls):
+                    cls_info_res = cls_info
+                    break
+            else:  # non in responsible item classes
+                return
+            keys = cls_info_res['keys']
+            res = self.search_item(item, cls_info_res)
+            if not keys or not res:  # no keys for checking or not exists
+                output = item.serializer() if hasattr(item, 'serializer') else dict(item)
+                spider.crawler.stats.inc_value('pipeline/es_item_insert_cnt')
+                self.bulk_item(output, cls_info_res)
+            else:
+                set_dict = {key: item[key] for key in cls_info_res['nullable_fields']
+                            if res.get(key) and not item.get(key, None)}
+                if set_dict:
+                    self.bulk_item({'_id': res.get('_id'), **set_dict}, cls_info_res, 'update')
+                    spider.crawler.stats.inc_value('pipeline/es_item_update_cnt')
+            return item
